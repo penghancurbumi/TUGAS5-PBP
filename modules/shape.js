da9f70d211b50e3d9e9562cb5b8f522b0d1fcf31
penghancurbumi <161696524+penghancurbumi@users.noreply.github.com>
2024-10-24 18:33:40 +0700

    Add files via upload

--- modules/shape.js
+++ modules/shape.js
@@ -1,1 +1,32 @@
+const persegi = {
+    luas: (sisi) => {
+        return sisi*sisi;
+    },
+    keliling: (sisi)  =>{
+        return 4*sisi;
+    }
+}
 
+const segitiga = {
+    luas: (alas,tinggi) => {
+        return 0.5*(alas*tinggi);
+    },
+    keliling: (A,B,C) =>{
+        return A+B+C;
+    }
+}
+
+const persegipanjang ={
+    luas: (panjang,lebar) =>{
+        return panjang*lebar
+    },
+    keliling:(panjang,lebar) => {
+        return 2*(panjang+lebar)
+    }
+}
+
+module.exports = {
+    persegi,
+    segitiga,
+    persegipanjang,
+}
